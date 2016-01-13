7667713c9946c7db4cbcc9f4ac7eb93f79840752
annapodobrazhnykh <annapodobrazhnykh@yandex.ru>
2016-01-13 14:47:44 +0300

    new lalki

---
+++ Demo1/Record.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Record
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        
+    }
+}
